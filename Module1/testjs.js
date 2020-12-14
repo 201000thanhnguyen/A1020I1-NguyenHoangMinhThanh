0406433ab36257283f2eef6e7fafa0562e828a68
201000thanhnguyen <201000thanhnguyen@gmail.com>
2020-12-14 20:02:35 +0700

    ver 19

--- Module1/testjs.js
+++ Module1/testjs.js
@@ -1,1 +1,25 @@
-var string2Num=parseInt(""123xyz"");
+function add(a, b) {
+return a + b;
+}
+
+var sum = add("4", "3");
+console.log(sum);
+
+var ep = "1";
+var epkieuNumber = Number(ep);
+var epkieuBoolean = Boolean(ep);
+
+
+console.log(typeof(ep));
+console.log(typeof(epkieuNumber));
+console.log(typeof(epkieuBoolean));
+
+var nhayDon = 'xin chào "minh thành1"';
+console.log(nhayDon);
+var nhayKep = "xin chào 'minh thành2'";
+console.log(nhayKep);
+
+for (var i = 0; i < 3; i++) {
+	console.log(i);
+}
+
