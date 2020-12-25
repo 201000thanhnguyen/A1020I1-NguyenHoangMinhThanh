154503af19552c244c02a337a7b592e82a31208e
201000thanhnguyen <201000thanhnguyen@gmail.com>
2020-12-25 13:52:35 +0700

    ver 21

--- Module1/testjs.js
+++ Module1/testjs.js
@@ -1,25 +1,2 @@
-function add(a, b) {
-return a + b;
-}
-
-var sum = add("4", "3");
-console.log(sum);
-
-var ep = "1";
-var epkieuNumber = Number(ep);
-var epkieuBoolean = Boolean(ep);
-
-
-console.log(typeof(ep));
-console.log(typeof(epkieuNumber));
-console.log(typeof(epkieuBoolean));
-
-var nhayDon = 'xin chào "minh thành1"';
-console.log(nhayDon);
-var nhayKep = "xin chào 'minh thành2'";
-console.log(nhayKep);
-
-for (var i = 0; i < 3; i++) {
-	console.log(i);
-}
-
+var date = new Date();
+console.log(date);
