ad86184200c8881ffdcef05372b9bc465dcdc87b
201000thanhnguyen <201000thanhnguyen@gmail.com>
2021-02-25 21:14:47 +0700

    java ver 20

--- Module2/src/_11_Stack_And_Queue/_bai_tap/Call.java
+++ Module2/src/_11_Stack_And_Queue/_bai_tap/Call.java
@@ -1,11 +1,18 @@
 package _11_Stack_And_Queue._bai_tap;
 
 import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Call {
     public static void main(String[] args) {
-        int[] mangTesst = new int[]{1,2,3,4};
+        int[] mangTesst = new int[]{1,2,3,4,9};
         mangTesst = ReverseArrayWithStack.reverseArray(mangTesst);
         System.out.println(Arrays.toString(mangTesst));
+
+        String chuoiString = "abc zyx bd 3d";
+        System.out.println(
+                chuoiString = ReverseCharWithStack.reverseChar(chuoiString)
+        );
     }
 }
--- Module2/src/_11_Stack_And_Queue/_bai_tap/ReverseCharWithStack.java
+++ Module2/src/_11_Stack_And_Queue/_bai_tap/ReverseCharWithStack.java
@@ -6,29 +6,18 @@ import java.util.Stack;
 public class ReverseCharWithStack {
 
     public static String reverseChar(String str) {
-        char[] charNew = str.toCharArray();
+        char[] charLength = str.toCharArray();
+        char[] charNew = new char[charLength.length];
         Stack<Character> stackChar = new Stack<>();
-        for (int i = charNew.length-1; i >= 0; i--){
-            stackChar.push(charNew[i]);
-            for (int j = 0; j < charNew.length; j++){
-                if (j + i+1 == charNew.length){
+        for (int i = charLength.length-1; i >= 0; i--){
+            stackChar.push(charLength[i]);
+            for (int j = 0; j < charLength.length; j++){
+                if (j + i+1 == charLength.length){
                     charNew[j] = stackChar.get(j);
                 }
             }
         }
-        return "chưa xong :V";
-    }
-
-    public static void main(String[] args) {
-        String chuoi = "a bc def";
-        char[] test_arr = chuoi.toCharArray();
-
-        Stack<Character> stackChar = new Stack<>();
-
-
-
-        String a = new String(test_arr);
-        System.out.println(a);
-        System.out.println(Arrays.toString(test_arr));
+        String stringNew = new String(charNew);
+        return stringNew;
     }
 }
