79e85a69212083f48f0b102ca30bb3e963e0417a
201000thanhnguyen <201000thanhnguyen@gmail.com>
2021-11-12 17:18:58 +0700

    1

---
+++ Module6/BezkoderFirebase/Angular12FirebaseCrud/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDhUmxUXDJa1JWBBF7MIBb-Mx8g5i08nUY",
+    authDomain: "bezkoder-firebase-8a2d9.firebaseapp.com",
+    databaseURL: "https://bezkoder-firebase-8a2d9-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "bezkoder-firebase-8a2d9",
+    storageBucket: "bezkoder-firebase-8a2d9.appspot.com",
+    messagingSenderId: "1065103956130",
+    appId: "1:1065103956130:web:02a9d5fe658b0ab2491ffb"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
